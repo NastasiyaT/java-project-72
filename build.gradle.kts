3fff6fb41441c4b5a7b8d03da148475db7062824
Anastasiya Trusova <129950255+NastasiyaT@users.noreply.github.com>
2023-10-11 21:24:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,3 +43,15 @@ tasks.test {
         showStandardStreams = true
     }
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "hexlet.code.App"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
