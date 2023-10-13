6a27da0da22e039e2c867d46ecfe26fdc0d9f261
Nastasiya Trusova <trusova.ab@yandex.ru>
2023-10-13 10:45:52 +0300

    add layout and main page template

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,13 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.repository.BaseRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -19,20 +23,31 @@ import java.util.stream.Collectors;
 public class App {
 
     private static String getDatabaseUrl() {
-
         return System.getenv()
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:webapp;DB_CLOSE_DELAY=-1;");
+    }
+
+    private static InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = App.class.getClassLoader();
+        return classLoader.getResourceAsStream(fileName);
+    }
+
+    private static String getContentFromStream(InputStream is) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
-    }
-
-    public static void main(String[] args) throws IOException, SQLException {
-        var app = getApp();
-
-        app.start(getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
@@ -55,20 +70,18 @@ public class App {
             config.plugins.enableDevLogging();
         });
 
-        app.get(NamedRoutes.rootPath(), ctx -> ctx.result("Hello World"));
+        JavalinJte.init(createTemplateEngine());
+
+        app.before(ctx -> ctx.contentType("text/html; charset=utf-8"));
+
+        app.get(NamedRoutes.rootPath(), ctx -> ctx.render("templates/mainpage.jte"));
 
         return app;
     }
 
-    private static InputStream getFileFromResourceAsStream(String fileName) {
-        ClassLoader classLoader = App.class.getClassLoader();
-        return classLoader.getResourceAsStream(fileName);
-    }
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
 
-    private static String getContentFromStream(InputStream is) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining("\n"));
-        }
+        app.start(getPort());
     }
-
 }
