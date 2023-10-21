545632df3995208f2142f782e657c0dfb62bdb0f
Anastasiya Trusova <129950255+NastasiyaT@users.noreply.github.com>
2023-10-21 13:12:27 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,7 +26,7 @@ public class App {
 
     private static String getDatabaseUrl() {
         return System.getenv()
-                .getOrDefault("JDBC_ENV", "jdbc:h2:mem:webapp;DB_CLOSE_DELAY=-1;");
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:webapp;DB_CLOSE_DELAY=-1;");
     }
 
     private static InputStream getFileFromResourceAsStream(String fileName) {
