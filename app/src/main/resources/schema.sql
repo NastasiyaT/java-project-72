9e71add73411a9c3e42fda03f390c791338b0dd2
Anastasiya Trusova <129950255+NastasiyaT@users.noreply.github.com>
2023-10-22 10:49:01 +0300

    Update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS url_checks;
-DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS urls CASCADE;
 
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL
 );
 
+DROP TABLE IF EXISTS url_checks;
 CREATE TABLE url_checks (
-    id SERIAL PRIMARY KEY,
-    url_id INTEGER REFERENCES urls(id),
-    status_code INTEGER,
-    h1 VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls (id) NOT NULL,
+    status_code BIGINT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
     description TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL
 )
